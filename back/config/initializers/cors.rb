ac61778c54eebc0a38a82899acaf63b85b7a17b8
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-02-05 19:34:35 +0900

    update: 環境構築設定を修正

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000', '127.0.0.1:8000', "https://idea-space-trip-front-g8anhufcz-meimeis-projects.vercel.app/"
+    origins 'localhost:4000', '127.0.0.1:4000', "https://idea-space-trip-front-g8anhufcz-meimeis-projects.vercel.app/"
 
     resource "*",
       headers: :any,
