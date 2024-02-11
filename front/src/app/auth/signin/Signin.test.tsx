725d9c41b532fb1cc8e5a5e8155c18d1bc4d1275
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-02-12 00:08:24 +0900

    fix: テストが成功するよう修正

---
+++ front/src/app/auth/signin/Signin.test.tsx
@@ -0,0 +1,29 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import Signin from "@/app/auth/signin/page";
+
+describe("Signin", () => {
+  it("should render the sign in page", () => {
+    render(<Signin />);
+    // タイトルが正しく表示されているかを検証
+    expect(screen.getByText("Sign In")).toBeInTheDocument();
+
+    // Google サインインボタンが存在するかを検証
+    expect(
+      screen.getByRole("button", { name: "Sign in with Google" }),
+    ).toBeInTheDocument();
+
+    // ゲストサインインボタンが存在するかを検証
+    expect(
+      screen.getByRole("button", { name: "Sign in as Guest" }),
+    ).toBeInTheDocument();
+
+    // 利用規約とプライバシーポリシーへのリンクが存在するかを検証
+    expect(screen.getByText("利用規約")).toHaveAttribute("href", "/terms");
+    expect(screen.getByText("プライバシーポリシー")).toHaveAttribute(
+      "href",
+      "/privacy",
+    );
+  });
+});
---
+++ front/src/components/SignInButton/SignInButton.test.tsx
@@ -0,0 +1,42 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import SignInButton from "@/components/SignInButton/SignInButton";
+import { signIn } from "next-auth/react";
+
+jest.mock("next-auth/react", () => ({
+  // signIn 関数を Jest のモック関数 (jest.fn()) に置き換え
+  // signIn 関数はログインに成功すると、Promise を返す
+  signIn: jest.fn(() => Promise.resolve()),
+}));
+
+describe("SignInButton", () => {
+  it("should render Google sign-in button correctly", () => {
+    render(<SignInButton provider="google" />);
+    expect(
+      screen.getByRole("button", { name: "Sign in with Google" }),
+    ).toBeInTheDocument();
+  });
+
+  it("should render guest sign-in button correctly", () => {
+    render(<SignInButton provider="credentials" />);
+    expect(
+      screen.getByRole("button", { name: "Sign in as Guest" }),
+    ).toBeInTheDocument();
+  });
+
+  it("should call signIn function when Google sign-in button is clicked", async () => {
+    render(<SignInButton provider="google" />);
+    const button = screen.getByRole("button", { name: "Sign in with Google" });
+    await userEvent.click(button); // ログイン処理が終わるのを待ってから次の処理を実行
+    expect(signIn).toHaveBeenCalledWith("google"); // signIn関数が引数 "google" で呼び出されたかどうかを検証
+  });
+
+  it("should call signIn function when Guest sign-in button is clicked", async () => {
+    render(<SignInButton provider="credentials" />);
+    const button = screen.getByRole("button", { name: "Sign in as Guest" });
+    await userEvent.click(button); // ログイン処理が終わるのを待ってから次の処理を実行
+    expect(signIn).toHaveBeenCalledWith("credentials"); // signIn関数が引数 "credentials" で呼び出されたかどうかを検証
+  });
+});
