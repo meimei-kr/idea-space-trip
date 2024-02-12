3fb70d39c7030d45b75cbed831136429c963f0b4
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-02-12 16:06:23 +0900

    update: worker用設定、メモリ変更

--- back/config/puma.rb
+++ back/config/puma.rb
@@ -16,6 +16,8 @@ if ENV['RAILS_ENV'] == 'production'
   require 'concurrent-ruby'
   worker_count = Integer(ENV.fetch('WEB_CONCURRENCY') { Concurrent.physical_processor_count })
   workers worker_count if worker_count > 1
+
+  plugin :solid_queue
 end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
