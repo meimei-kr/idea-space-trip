3116761e5aae6c3c05a468a9c782f7a770226077
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-02-20 02:34:10 +0900

    fix: CSS修正 #16

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -65,10 +65,10 @@ export default async function Home() {
             <p className={styles.description}>
               そんな風に思いながら、具体的にどんなことをしたらいいのかアイデアに詰まってしまっていませんか？
             </p>
-            <p>
+            <p className={styles.description}>
               このアプリなら、有名なアイデア発想法に沿って、AIのサポートを受けながらアイデア出しが行なえます。
             </p>
-            <p>
+            <p className={styles.description}>
               さぁ、IDEA SPACE TRIPで、一緒に創造性の宇宙を旅してみましょう！
             </p>
           </div>
