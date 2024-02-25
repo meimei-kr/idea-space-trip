0c2ae23be57c0c9cd3a0e07800c9457023dea8da
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-02-26 00:35:25 +0900

    fix: route.tsをもとに戻す

--- front/src/app/api/auth/[...nextauth]/route.ts
+++ front/src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,6 @@
-export { GET, POST } from "@/auth";
+import { authOptions } from "@/lib/options";
+import NextAuth from "next-auth";
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
