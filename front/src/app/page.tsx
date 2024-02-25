fed557a1a8164891614110f98e285678aaf7d9a3
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-02-26 01:28:55 +0900

    fix: pageコンポーネントのエクスポートの書き方を修正

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -2,10 +2,8 @@ import { HomePresentation } from "@/app/presentation/Home/HomePresentation";
 import { authOptions } from "@/lib/options";
 import { getServerSession } from "next-auth";
 
-export async function HomeContainer() {
+export default async function HomeContainer() {
   const session = await getServerSession(authOptions);
 
   return <HomePresentation session={session} />;
 }
-
-export default HomeContainer;
