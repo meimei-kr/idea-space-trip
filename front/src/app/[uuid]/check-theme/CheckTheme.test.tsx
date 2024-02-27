efc69d8e95f1e58fbc2cd2196d049e40de697bba
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-02-27 19:15:04 +0900

    add: テーマ有無選択画面フロントテスト追加 #18

---
+++ front/src/app/[uuid]/check-theme/CheckTheme.test.tsx
@@ -0,0 +1,37 @@
+import CheckTheme from "@/app/[uuid]/check-theme/page";
+import { render, screen } from "@testing-library/react";
+
+jest.mock("@/components/elements/LinkButton/LinkButton", () => ({
+  __esModule: true,
+  default: ({ children }: { children: React.ReactNode }) => (
+    <div>{children}</div>
+  ),
+}));
+
+jest.mock("@/components/elements/BackButton/BackButton", () => ({
+  __esModule: true,
+  default: () => <div>BackButton</div>,
+}));
+
+describe("CheckTheme", () => {
+  it("should render the sentence correctly", () => {
+    render(<CheckTheme />);
+
+    expect(
+      screen.getByText("考えたいテーマは、すでに決まってる？"),
+    ).toBeInTheDocument();
+  });
+
+  it("should display yes and no options", () => {
+    render(<CheckTheme />);
+
+    expect(screen.getByText("◯")).toBeInTheDocument();
+    expect(screen.getByText("✕")).toBeInTheDocument();
+  });
+
+  it("should display the BackButton", () => {
+    render(<CheckTheme />);
+
+    expect(screen.getByText("BackButton")).toBeInTheDocument();
+  });
+});
