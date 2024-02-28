39443034ddba4b5ccb9262358e4c2f95b4be6cb4
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-02-29 01:11:13 +0900

    update: schemaコメント追加 #18

--- back/spec/models/idea_session_spec.rb
+++ back/spec/models/idea_session_spec.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: idea_sessions
+#
+#  id                     :bigint           not null, primary key
+#  user_id                :bigint           not null
+#  uuid                   :string(255)      not null
+#  is_theme_determined    :boolean          default(FALSE), not null
+#  is_ai_theme_generated  :boolean          default(FALSE), not null
+#  category               :integer          default(0), not null
+#  question               :integer          default(0), not null
+#  is_ai_answer_generated :boolean          default(FALSE), not null
+#  theme                  :text(65535)
+#  is_finished            :boolean          default(FALSE), not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe IdeaSession do
