2138f15ff8c98afeac0a65ec6421e6ac0d6a4983
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-03-12 04:25:09 +0900

    update: LitUpBordersのLargeサイズを追加

--- front/src/components/ui/tailwind-buttons.tsx
+++ front/src/components/ui/tailwind-buttons.tsx
@@ -14,33 +14,38 @@ export function LitUpBorders({
   disabled,
 }: TailwindButtonProps) {
   return (
-    <>
-      {disabled ? (
-        <button
-          type={type}
-          className="p-[3px] relative opacity-50"
-          onClick={onClick}
-          disabled={disabled}
-        >
-          <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-          <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white">
-            {children}
-          </div>
-        </button>
-      ) : (
-        <button
-          type={type}
-          className="p-[3px] relative"
-          onClick={onClick}
-          disabled={disabled}
-        >
-          <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-          <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-            {children}
-          </div>
-        </button>
-      )}
-    </>
+    <button
+      type={type}
+      className={`p-[2px] relative ${disabled ? "opacity-50" : ""}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-full" />
+      <div className="px-8 py-2  bg-black rounded-full relative group transition duration-200 text-white text-md hover:bg-transparent">
+        {children}
+      </div>
+    </button>
+  );
+}
+
+export function LitUpBordersLg({
+  children,
+  onClick,
+  type,
+  disabled,
+}: TailwindButtonProps) {
+  return (
+    <button
+      type={type}
+      className={`p-[2px] relative ${disabled ? "opacity-50" : ""}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-full" />
+      <div className="px-12 py-3  bg-black rounded-full relative group transition duration-200 text-white hover:bg-transparent">
+        {children}
+      </div>
+    </button>
   );
 }
 
@@ -53,19 +58,19 @@ export function BorderMagic({
   return (
     <button
       type={type}
-      className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+      className="relative inline-flex h-36 w-36 overflow-hidden rounded-full p-0.5"
       onClick={onClick}
       disabled={disabled}
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-2 text-base font-medium text-white backdrop-blur-3xl hover:bg-transparent">
         {children}
       </span>
     </button>
   );
 }
 
-export function BackButton({
+export function Playlist({
   children,
   onClick,
   type,
@@ -74,7 +79,7 @@ export function BackButton({
   return (
     <button
       type={type}
-      className="shadow-[inset_0_0_0_2px_#616467] px-12 py-4 rounded-full tracking-widest uppercase font-normal text-xs bg-transparent hover:bg-[#616467] hover:text-white transition duration-200"
+      className="shadow-[inset_0_0_0_2px_#616467] px-8 py-3 rounded-full tracking-widest uppercase font-normal text-xs bg-transparent hover:bg-[#616467] hover:text-white transition duration-200"
       onClick={onClick}
       disabled={disabled}
     >
@@ -101,7 +106,7 @@ export function Shimmer({
   );
 }
 
-export function Simle({
+export function Simple({
   children,
   onClick,
   type,
@@ -115,6 +120,45 @@ export function Simle({
       disabled={disabled}
     >
       {children}
+    </button>
+  );
+}
+
+export function TailwindcssConnect({
+  children,
+  onClick,
+  type,
+  disabled,
+}: TailwindButtonProps) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block"
+    >
+      <span className="absolute inset-0 overflow-hidden rounded-full">
+        <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+      </span>
+      <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+        <span>{children}</span>
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="M10.75 8.75L14.25 12L10.75 15.25"
+          ></path>
+        </svg>
+      </div>
+      <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
     </button>
   );
 }
