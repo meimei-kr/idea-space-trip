28011d0f65a4a58baaf119e4dfdc67600bebfa1d
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-03-11 15:30:55 +0900

    update: 共通エラーページ修正

--- front/src/app/error.tsx
+++ front/src/app/error.tsx
@@ -1,26 +1,30 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Error({
   error,
-  reset,
 }: {
   error: Error & { digest?: string };
-  reset: () => void;
 }) {
+  const router = useRouter();
   useEffect(() => {
     console.error(error);
   }, [error]);
 
+  useEffect(() => {
+    router.prefetch("/");
+  });
+
   return (
-    <main className="flex h-full flex-col items-center justify-center">
+    <main className="flex h-screen flex-col items-center justify-center relative z-10">
       <h2 className="text-center">Something went wrong!</h2>
       <button
-        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
-        onClick={() => reset()}
+        className="mt-4 rounded-md bg-white px-4 py-2 text-sm text-slate-900 transition-colors hover:bg-blue-400"
+        onClick={() => router.push("/")}
       >
-        Try again
+        Top
       </button>
     </main>
   );
