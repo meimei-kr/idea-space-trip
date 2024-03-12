790a7d49e96f7556009852579cf7765d920f5d0a
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-03-12 15:18:20 +0900

    add: Action Cable設定追加 #23

--- back/config/environments/production.rb
+++ back/config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [%r{https://idea-space-trip\.*}]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
