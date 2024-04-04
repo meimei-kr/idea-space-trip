6d61ee9d99a461bcacbffe3c6c86c9713787a8df
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-04-04 17:55:15 +0900

    fix: ページコンポーネント名修正

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -2,7 +2,7 @@ import { HomePresentation } from "@/app/presentation/Home/HomePresentation";
 import { authOptions } from "@/lib/options";
 import { getServerSession } from "next-auth";
 
-export default async function HomeContainer() {
+export default async function page() {
   const session = await getServerSession(authOptions);
 
   return <HomePresentation session={session} />;
