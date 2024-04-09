cf44de2d8a5ac8aff0ebc606bd12810d98eb9a4d
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-04-09 16:46:23 +0900

    refactor: マイページ画面Container/Presentationalパターン廃止 #175

--- front/src/app/my-page/page.tsx
+++ front/src/app/my-page/page.tsx
@@ -1,8 +1,18 @@
+import styles from "@/app/my-page/MyPage.module.scss";
+import DeleteForm from "@/features/my-page/DeleteForm/DeleteForm";
 import { getUser } from "@/lib/user";
-import MyPagePresentation from "./MyPagePresentation";
 
 export default async function MyPage() {
   const user = await getUser();
 
-  return <MyPagePresentation user={user} />;
+  return (
+    <main className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>ユーザー設定</h1>
+          <DeleteForm user={user} />
+        </div>
+      </div>
+    </main>
+  );
 }
