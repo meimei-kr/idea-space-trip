cf07dfc7b00c37eee2d590eeb1ce279763490acc
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-05-03 00:15:19 +0900

    add: IdeaLikeモデルスペック追加 #125

--- back/spec/models/idea_like_spec.rb
+++ back/spec/models/idea_like_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe IdeaLike do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:idea_memo) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:idea_memo_id) }
+  end
 end
