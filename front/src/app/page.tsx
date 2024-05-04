607a31429a650b9b474ed6aafc17c14ef461fe12
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-05-04 19:09:53 +0900

    improve: トップページを静的Routeに変更 #199

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,17 +1,13 @@
 import styles from "@/app/Home.module.scss";
 import * as Home from "@/features/home/components";
-import { authOptions } from "@/lib/options";
-import { getServerSession } from "next-auth";
 
 export default async function page() {
-  const session = await getServerSession(authOptions);
-
   return (
     <main className={styles.main}>
       <Home.HeroSection />
       <Home.AboutSection />
       <Home.FeaturesSection />
-      <Home.LoginSection session={session} />
+      <Home.LoginSection />
     </main>
   );
 }
